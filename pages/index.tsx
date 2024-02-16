c0aaeeeecec7fb05d40f250eaeec7fff2152d326
lux18 <iwaldiputra@gmail.com>
2024-02-16 09:17:36 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import { NextPage } from "next";
+import Staking from "./staking";
 
 const Home: NextPage = () => {
   return (
-    <div className="container">
-      <h1>Home</h1>
+    <div>
+      <Staking />
     </div>
   );
 };
